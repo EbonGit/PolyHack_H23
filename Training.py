0b9c1d9ef128d9885b09ef1ce95b9c7e24c6d689
Kephren <kephren.delannay.sampany@gmail.com>
2023-02-04 16:39:36 -0500

    finished training interface

---
+++ Training.py
@@ -0,0 +1,94 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout
+
+IMAGE_SIZE = 170
+
+class Ui_MainWindow(object):
+
+    def __init__(self):
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.horizontalLayout = QHBoxLayout(self.centralwidget)
+        self.verticalLayout = QVBoxLayout()
+
+        self.label_image_top = QtWidgets.QLabel(self.centralwidget)
+        self.label_image_bottom = QtWidgets.QLabel(self.centralwidget)
+        self.label_image_shoes = QtWidgets.QLabel(self.centralwidget)
+
+        self.nope_button = QtWidgets.QPushButton(self.centralwidget)
+        self.yes_button = QtWidgets.QPushButton(self.centralwidget)
+
+        self.image_top_index = None
+        self.image_bottom_index = None
+        self.image_shoes_index = None
+
+
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(414, 736)
+        MainWindow.setWindowTitle("Training")
+
+        # setup central widget
+        self.centralwidget.setObjectName("centralwidget")
+        self.centralwidget.resize(414, 736)
+
+        # setup layouts
+        self.horizontalLayout.setObjectName("horizontalLayoutWidget")
+        self.verticalLayout.setObjectName("verticalLayoutWidget")
+
+        # setup images*
+        self.label_image_top.setAlignment(Qt.AlignCenter)
+        self.label_image_bottom.setAlignment(Qt.AlignCenter)
+        self.label_image_shoes.setAlignment(Qt.AlignCenter)
+
+        self.image_top_index = 0
+        self.image_bottom_index = 0
+        self.image_shoes_index = 0
+        self.loadImages("tshirt.jpg", "short.jpg", "shoes.jpg")
+
+        # setup buttons
+        self.nope_button.clicked.connect(lambda : self.feedback(False))
+        self.nope_button.setText("Nope")
+        self.yes_button.clicked.connect(lambda: self.feedback(True))
+        self.yes_button.setText("Yeet")
+
+        # add to layouts
+        self.verticalLayout.addWidget(self.label_image_top)
+        self.verticalLayout.addWidget(self.label_image_bottom)
+        self.verticalLayout.addWidget(self.label_image_shoes)
+        self.horizontalLayout.addWidget(self.nope_button)
+        self.horizontalLayout.addLayout(self.verticalLayout)
+        self.horizontalLayout.addWidget(self.yes_button)
+
+    def feedback(self, result):
+        if result:
+            print(f"J'aime ! "
+                  f"\n top_index : {self.image_top_index},"
+                  f"\n bottom_index : {self.image_bottom_index},"
+                  f"\n top_index : {self.image_shoes_index} ")
+        else:
+            print(f"Je n'aime pas !"
+                  f"\n top_index : {self.image_top_index},"
+                  f"\n bottom_index : {self.image_bottom_index},"
+                  f"\n top_index : {self.image_shoes_index} ")
+
+    def loadImages(self, top, bottom, shoes):
+        self.pixmap1 = QtGui.QPixmap(top)
+        self.pixmap2 = QtGui.QPixmap(bottom)
+        self.pixmap3 = QtGui.QPixmap(shoes)
+        self.label_image_top.setPixmap(
+            self.pixmap1.scaled(IMAGE_SIZE, IMAGE_SIZE, Qt.KeepAspectRatio, Qt.FastTransformation))
+        self.label_image_bottom.setPixmap(
+            self.pixmap2.scaled(IMAGE_SIZE, IMAGE_SIZE, Qt.KeepAspectRatio, Qt.FastTransformation))
+        self.label_image_shoes.setPixmap(
+            self.pixmap3.scaled(IMAGE_SIZE, IMAGE_SIZE, Qt.KeepAspectRatio, Qt.FastTransformation))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
