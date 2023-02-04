6008876bb522dc4aebc7ec4fbad6cd84b8e45531
Kephren <kephren.delannay.sampany@gmail.com>
2023-02-04 18:02:25 -0500

    updated training interface and added sort of backend

--- Training.py
+++ Training.py
@@ -2,11 +2,18 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout
 
+import pandas as pd
+import random
+
 IMAGE_SIZE = 170
 
 class Ui_MainWindow(object):
 
     def __init__(self):
+
+        self.data = pd.read_csv("out.csv")
+        print(self.data.head())
+
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.horizontalLayout = QHBoxLayout(self.centralwidget)
         self.verticalLayout = QVBoxLayout()
@@ -17,11 +24,13 @@ class Ui_MainWindow(object):
 
         self.nope_button = QtWidgets.QPushButton(self.centralwidget)
         self.yes_button = QtWidgets.QPushButton(self.centralwidget)
+        self.close_button = QtWidgets.QPushButton(self.centralwidget)
 
-        self.image_top_index = None
-        self.image_bottom_index = None
-        self.image_shoes_index = None
-
+        self.index = random.randint(0, 99)
+        self.getImages()
+        self.image_top_index = self.images_indexes["top"]
+        self.image_bottom_index = self.images_indexes["bottom"]
+        self.image_shoes_index = self.images_indexes["shoes"]
 
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
@@ -41,41 +50,52 @@ class Ui_MainWindow(object):
         self.label_image_bottom.setAlignment(Qt.AlignCenter)
         self.label_image_shoes.setAlignment(Qt.AlignCenter)
 
-        self.image_top_index = 0
-        self.image_bottom_index = 0
-        self.image_shoes_index = 0
-        self.loadImages("tshirt.jpg", "short.jpg", "shoes.jpg")
-
         # setup buttons
         self.nope_button.clicked.connect(lambda : self.feedback(False))
         self.nope_button.setText("Nope")
         self.yes_button.clicked.connect(lambda: self.feedback(True))
         self.yes_button.setText("Yeet")
+        self.close_button.clicked.connect(self.save)
+        self.close_button.setText("Done")
 
         # add to layouts
         self.verticalLayout.addWidget(self.label_image_top)
         self.verticalLayout.addWidget(self.label_image_bottom)
         self.verticalLayout.addWidget(self.label_image_shoes)
+        self.verticalLayout.addWidget(self.close_button)
         self.horizontalLayout.addWidget(self.nope_button)
         self.horizontalLayout.addLayout(self.verticalLayout)
         self.horizontalLayout.addWidget(self.yes_button)
 
+
     def feedback(self, result):
         if result:
+            self.data["result"].iloc[self.index] = 1
             print(f"J'aime ! "
                   f"\n top_index : {self.image_top_index},"
                   f"\n bottom_index : {self.image_bottom_index},"
                   f"\n top_index : {self.image_shoes_index} ")
         else:
+            self.data["result"].iloc[self.index] = 0
             print(f"Je n'aime pas !"
                   f"\n top_index : {self.image_top_index},"
                   f"\n bottom_index : {self.image_bottom_index},"
                   f"\n top_index : {self.image_shoes_index} ")
 
+
+        self.getImages()
+
+
     def loadImages(self, top, bottom, shoes):
-        self.pixmap1 = QtGui.QPixmap(top)
-        self.pixmap2 = QtGui.QPixmap(bottom)
-        self.pixmap3 = QtGui.QPixmap(shoes)
+
+        top_path = f"images/t_{top}.jpg"
+        bottom_path = f"images/short_{bottom}.jpg"
+        shoes_path = f"images/shoes_{shoes}.jpg"
+
+
+        self.pixmap1 = QtGui.QPixmap(top_path)
+        self.pixmap2 = QtGui.QPixmap(bottom_path)
+        self.pixmap3 = QtGui.QPixmap(shoes_path)
         self.label_image_top.setPixmap(
             self.pixmap1.scaled(IMAGE_SIZE, IMAGE_SIZE, Qt.KeepAspectRatio, Qt.FastTransformation))
         self.label_image_bottom.setPixmap(
@@ -83,6 +103,17 @@ class Ui_MainWindow(object):
         self.label_image_shoes.setPixmap(
             self.pixmap3.scaled(IMAGE_SIZE, IMAGE_SIZE, Qt.KeepAspectRatio, Qt.FastTransformation))
 
+    def getImages(self):
+        self.index = random.randint(0, 99)
+        self.images_indexes = self.data.iloc[self.index]
+        self.image_top_index = self.images_indexes["top"]
+        self.image_bottom_index = self.images_indexes["bottom"]
+        self.image_shoes_index = self.images_indexes["shoes"]
+        self.loadImages(self.image_top_index, self.image_bottom_index, self.image_shoes_index)
+
+    def save(self):
+        print('updates done to csv at line : ', self.index)
+        self.data.to_csv('out.csv')
 
 if __name__ == "__main__":
     import sys
--- generate_data.py
+++ generate_data.py
@@ -5,25 +5,27 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 
-nTops = 10
-nBottoms = 12
-nShoes = 6
+nTops = 3
+nBottoms = 3
+nShoes = 3
 
 df = pd.DataFrame({'top': [], 'bottom': [], 'shoes': [], 'result': []})
 print(df.describe())
 
-for j in range(200):
+for j in range(100):
     top = random.randint(1, nTops)
     bottom = random.randint(1, nBottoms)
     shoes = random.randint(1, nShoes)
 
     result = 0
 
-    if ((top + bottom + shoes) % 2 == 0):
-        result = 1
+    # if ((top + bottom + shoes) % 2 == 0):
+    #     result = 1
 
     new_row = pd.DataFrame({'top' : [top], 'bottom' : [bottom], 'shoes' : [shoes], 'result' : [result]})
     df = pd.concat([df, new_row])
 
 print(df.describe())
+df = df.fillna(0)
+df = df.astype(int)
 df.to_csv("out.csv", index=False)
