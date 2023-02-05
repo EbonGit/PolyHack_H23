bef6eecca76a8d9000d7154b7c494990769756e7
Kephren <kephren.delannay.sampany@gmail.com>
2023-02-05 10:29:26 -0500

    C'est celle là (O.G)

--- generate_data.py
+++ generate_data.py
@@ -11,20 +11,23 @@ nShoes = 4
 
 def init():
 
-    df = pd.DataFrame({'top': [], 'bottom': [], 'shoes': [], 'result': []})
-    print(df.describe())
+    df = pd.read_csv('Prefab.csv')
+    df.to_csv('out.csv', index=False)
 
-    for i in range(1, 5):
-        for j in range(1, 5):
-            for k in range(1, 5):
-                new_row = pd.DataFrame({'top': [i], 'bottom': [j], 'shoes': [k], 'result': [-1]})
-                df = pd.concat([df, new_row])
-
-    print(df.describe())
-    df = df.fillna(0)
-    df = df.astype(int)
-    df = df.sample(frac=1).reset_index(drop=True)
-    df.to_csv("out.csv", index=False)
+    # df = pd.DataFrame({'top': [], 'bottom': [], 'shoes': [], 'result': []})
+    # print(df.describe())
+    #
+    # for i in range(1, 5):
+    #     for j in range(1, 5):
+    #         for k in range(1, 5):
+    #             new_row = pd.DataFrame({'top': [i], 'bottom': [j], 'shoes': [k], 'result': [-1]})
+    #             df = pd.concat([df, new_row])
+    #
+    # print(df.describe())
+    # df = df.fillna(0)
+    # df = df.astype(int)
+    # df = df.sample(frac=1).reset_index(drop=True)
+    # df.to_csv("out.csv", index=False)
 
 if __name__ == "__main__":
     init()
